2789711bc3eb088da8a1cedfd8107d010e3f4f9c
OSITA PC <ositaon2020@gmail.com>
2023-02-11 21:30:24 +0100

    Created ReadME.dart
    Supporting Document for Demo-repo2 Folder

---
+++ ReadME.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
